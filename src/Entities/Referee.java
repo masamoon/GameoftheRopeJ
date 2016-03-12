90d1cdc1772a09e104afaf8a9b4fff0783d0a008
andre.lopes@ua.pt <andre.lopes@ua.pt>
2016-03-12 22:42:24 +0000

    Proposal of basic life cycle flow

--- src/Entities/Referee.java
+++ src/Entities/Referee.java
@@ -1,5 +1,7 @@
 package Entities;
 
+import States.RefereeState;
+
 /**
  * Created by jonnybel on 3/8/16.
  */
@@ -7,6 +9,65 @@ public class Referee extends Thread {
 
     /* todo: add comments */
 
+    private RefereeState refstate;
+
+    /* Life Cycle of the Referee Thread
+    */
+
+    @Override
+    public void run(){
+
+        while(true){
+            callTrial();
+            waitingForTeamsReady();
+            startTrial();
+            waitingForTrialEnd();
+            assertTrialDecision();
+        }
+
+    }
 
 
+    public RefereeState getrefstate() {
+        return refstate;
+    }
+
+    public void setrefstate(RefereeState state) {
+        this.refstate = state;
+    }
+
+    /*
+    *   Referee calls the teams to assemble
+    */
+    public void callTrial(){
+
+    }
+
+    /*
+     *   Referee begins the trial
+    */
+    public void startTrial(){
+
+    }
+
+    /*
+     *Waiting for the last member of the teams to sit up */
+
+    public void waitingForTeamsReady(){
+        try
+        { sleep ((long) (1 + 40 * Math.random ()));
+        }
+        catch (InterruptedException e) {}
+    }
+
+    public void waitingForTrialEnd(){
+        try
+        { sleep ((long) (1 + 40 * Math.random ()));
+        }
+        catch (InterruptedException e) {}
+    }
+
+    public void assertTrialDecision(){
+
+    }
 }
